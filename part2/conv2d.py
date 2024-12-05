8ea82562f53933ee7febad5167093af1043102af
echen4628 <echen4628@gmail.com>
2024-12-05 00:32:51 +0000

    working for small images

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -317,6 +317,166 @@ The shape of the output should be [batch_size, out_channels, out_pool_height, ou
 #     return X_out
 
 
+# @nki.jit
+# def fused_conv2d_maxpool(X, W, bias, pool_size=1):
+#     batch_size, in_channels, input_height, input_width = X.shape
+#     out_channels, in_channels_, filter_height, filter_width = W.shape
+#     out_channels_ = bias.shape[0]
+
+#     assert (
+#         in_channels_ == in_channels and out_channels_ == out_channels
+#     ), f"Shape mismatch. {in_channels}, {in_channels_}, {out_channels}, {out_channels_}"
+
+#     out_height = input_height - filter_height + 1
+#     out_width = input_width - filter_width + 1
+
+#     out_pool_height = out_height // pool_size
+#     out_pool_width = out_width // pool_size
+    
+#     # Can assume multiple of 128 to avoid using mask
+#     assert in_channels % 128 == 0
+
+#     # Can assume one PSUM bank can at least fit one row of the pixels
+#     assert nl.tile_size.gemm_moving_fmax >= out_width
+
+#     # Initialize output array
+#     X_out = nl.ndarray(
+#         shape=(batch_size, out_channels, out_pool_height, out_pool_width),
+#         dtype=X.dtype,
+#         buffer=nl.hbm,
+#     )
+
+#     # Various tiling dimensions (You may want to define more of them)
+#     c_in_pmax = nl.tile_size.pmax
+#     n_tiles_c_in = in_channels // c_in_pmax
+#     n_tiles_c_out = out_channels // c_in_pmax
+
+#     out_channels_idx = nl.arange(nl.tile_size.pmax)[:,None,None]
+#     out_pool_height_idx = nl.arange(out_pool_height)[None,:,None]
+#     out_pool_width_idx = nl.arange(out_pool_width)[None,None,:]
+
+#     in_channels_idx = nl.arange(nl.tile_size.pmax)[:,None,None]
+#     out_height_idx = nl.arange(out_height)[None,:,None]
+#     out_width_idx = nl.arange(out_width)[None,None,:]
+
+#     # Process the images in batches
+#     for b in nl.affine_range(batch_size):
+#         for output_tile_idx in nl.affine_range(n_tiles_c_out):
+#             for input_row in nl.affine_range(out_pool_height//2):
+#                     # create the accumulator in psum
+#                     X_out_tile_before_pooling = nl.zeros((c_in_pmax, 2, out_width),  X_out.dtype, buffer=nl.psum)
+#                     for i in nl.affine_range(filter_height):
+#                         for input_tile_idx in nl.affine_range(n_tiles_c_in):
+#                             for j in nl.affine_range(filter_width):
+#                                 # shift row
+#                                 shifted_row = 2*input_row*pool_size+i
+#                                 X_rhs_tile = nl.load(X[b,c_in_pmax*input_tile_idx:c_in_pmax*(input_tile_idx+1), shifted_row:shifted_row+2, :])
+                                
+#                                 # shift the columns
+#                                 X_rhs_tile_shifted = X_rhs_tile[in_channels_idx, nl.arange(2)[None,:,None], out_width_idx+j]
+#                                 W_tile = nl.load(W[c_in_pmax*output_tile_idx:c_in_pmax*(output_tile_idx+1), c_in_pmax*input_tile_idx:c_in_pmax*(input_tile_idx+1)])
+                                
+#                                 # do matmul with W
+#                                 X_out_tile_before_pooling += nl.matmul(W_tile[:,:,i,j], X_rhs_tile_shifted, transpose_x=False)
+#                     # add bias
+#                     X_out_tile_before_pooling_with_bias = (nl.load(bias[output_tile_idx*c_in_pmax:(output_tile_idx+1)*(c_in_pmax),])+X_out_tile_before_pooling)
+                    
+#                     # perform maxpooling
+#                     temp1 = nl.copy(X_out_tile_before_pooling_with_bias.reshape((c_in_pmax, pool_size, 2/pool_size, out_width)))
+#                     temp2 = nl.max(temp1, axis=1)
+#                     temp3 = nl.copy(temp2.reshape((c_in_pmax, 2/pool_size, pool_size, out_width/pool_size)))
+#                     X_out_tile_after_pooling = nl.max(temp3, axis=2)
+#                     # X_out_tile_after_pooling = nl.max(nl.max(X_out_tile_before_pooling_with_bias.reshape((c_in_pmax, pool_size, 2/pool_size, out_width)), axis=1).reshape((c_in_pmax, 2/pool_size, pool_size, out_width/pool_size)), axis=2)
+#                     nl.store(X_out[b, c_in_pmax*output_tile_idx:c_in_pmax*(output_tile_idx+1), 2*input_row:(2*input_row+2)], value=X_out_tile_after_pooling)
+#     return X_out
+
+
+
+# @nki.jit
+# def fused_conv2d_maxpool(X, W, bias, pool_size=1):
+#     batch_size, in_channels, input_height, input_width = X.shape
+#     out_channels, in_channels_, filter_height, filter_width = W.shape
+#     out_channels_ = bias.shape[0]
+
+#     assert (
+#         in_channels_ == in_channels and out_channels_ == out_channels
+#     ), f"Shape mismatch. {in_channels}, {in_channels_}, {out_channels}, {out_channels_}"
+
+#     out_height = input_height - filter_height + 1
+#     out_width = input_width - filter_width + 1
+
+#     out_pool_height = out_height // pool_size
+#     out_pool_width = out_width // pool_size
+    
+#     # Can assume multiple of 128 to avoid using mask
+#     assert in_channels % 128 == 0
+
+#     # Can assume one PSUM bank can at least fit one row of the pixels
+#     assert nl.tile_size.gemm_moving_fmax >= out_width
+
+#     # Initialize output array
+#     X_out = nl.ndarray(
+#         shape=(batch_size, out_channels, out_pool_height, out_pool_width),
+#         dtype=X.dtype,
+#         buffer=nl.hbm,
+#     )
+
+#     # Various tiling dimensions (You may want to define more of them)
+#     c_in_pmax = nl.tile_size.pmax
+#     n_tiles_c_in = in_channels // c_in_pmax
+#     n_tiles_c_out = out_channels // c_in_pmax
+
+#     out_channels_idx = nl.arange(nl.tile_size.pmax)[:,None,None]
+#     out_pool_height_idx = nl.arange(out_pool_height)[None,:,None]
+#     out_pool_width_idx = nl.arange(out_pool_width)[None,None,:]
+
+#     in_channels_idx = nl.arange(nl.tile_size.pmax)[:,None,None]
+#     out_height_idx = nl.arange(out_height)[None,:,None]
+#     out_width_idx = nl.arange(out_width)[None,None,:]
+
+#     # Maximum free dimension of the stationary operand of general matrix multiplication on tensor engine
+#     TILE_WIDTH = 2*input_width
+
+#     # Maximum partition dimension of a tile
+#     TILE_128 = nl.tile_size.pmax  # 128
+
+#     # Process the images in batches
+#     for b in nl.affine_range(batch_size):
+#         for output_tile_idx in nl.affine_range(n_tiles_c_out):
+#             for input_row in nl.affine_range(out_pool_height//2):
+#                     # create the accumulator in psum
+#                     X_out_tile_before_pooling = nl.zeros((c_in_pmax, 2*out_width),  X_out.dtype, buffer=nl.psum)
+#                     for input_tile_idx in nl.affine_range(n_tiles_c_in):
+#                         for i in nl.affine_range(filter_height):
+#                             for j in nl.affine_range(filter_width):
+#                                         # Declare the tiles on SBUF
+#                                 W_tile = nl.ndarray((TILE_128, TILE_128, filter_height, filter_width), dtype=W.dtype, buffer=nl.sbuf)
+#                                 X_rhs_tile = nl.ndarray((TILE_128, TILE_WIDTH), dtype=X.dtype, buffer=nl.sbuf)
+#                                 X_rhs_tile_shifted = nl.ndarray((TILE_128, 2*out_width), dtype=X.dtype, buffer=nl.sbuf)
+#                                 # shift row
+#                                 shifted_row = 2*input_row*pool_size+i
+#                                 X_rhs_tile[...] = nl.load(X[b,c_in_pmax*input_tile_idx:c_in_pmax*(input_tile_idx+1), shifted_row:shifted_row+2, :]).reshape((TILE_128, TILE_WIDTH))
+#                                 pdb.set_trace()
+#                                 # shift the columns
+#                                 X_rhs_tile_shifted[...] = nl.copy(X_rhs_tile[in_channels_idx, nl.arange(2)[None,:,None]*input_width+(out_width_idx+j)]).reshape((TILE_128, 2*out_width))
+#                                 W_tile[...] = nl.load(W[c_in_pmax*output_tile_idx:c_in_pmax*(output_tile_idx+1), c_in_pmax*input_tile_idx:c_in_pmax*(input_tile_idx+1)])
+                                
+#                                 # do matmul with W
+#                                 X_out_tile_before_pooling += nl.matmul(W_tile[:,:,i,j], X_rhs_tile_shifted, transpose_x=False)
+#                     # add bias
+#                     X_out_tile_before_pooling_with_bias = (nl.load(bias[output_tile_idx*c_in_pmax:(output_tile_idx+1)*(c_in_pmax),])+nl.copy(X_out_tile_before_pooling))
+                    
+#                     # perform maxpooling
+#                     temp1 = nl.copy(X_out_tile_before_pooling_with_bias).reshape((c_in_pmax, pool_size, 2/pool_size, out_width))
+#                     temp2 = nl.max(temp1, axis=1)
+#                     temp3 = nl.copy(temp2).reshape((c_in_pmax, 2/pool_size, pool_size, out_width/pool_size))
+#                     X_out_tile_after_pooling = nl.max(temp3, axis=2)
+#                     # X_out_tile_after_pooling = nl.max(nl.max(X_out_tile_before_pooling_with_bias.reshape((c_in_pmax, pool_size, 2/pool_size, out_width)), axis=1).reshape((c_in_pmax, 2/pool_size, pool_size, out_width/pool_size)), axis=2)
+#                     nl.store(X_out[b, c_in_pmax*output_tile_idx:c_in_pmax*(output_tile_idx+1), 2*input_row:(2*input_row+2)], value=X_out_tile_after_pooling)
+#     return X_out
+
+
+
 @nki.jit
 def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     batch_size, in_channels, input_height, input_width = X.shape
@@ -359,34 +519,29 @@ def fused_conv2d_maxpool(X, W, bias, pool_size=1):
     out_height_idx = nl.arange(out_height)[None,:,None]
     out_width_idx = nl.arange(out_width)[None,None,:]
 
+    # Maximum free dimension of the stationary operand of general matrix multiplication on tensor engine
+    TILE_WIDTH = 2*input_width
+
+    # Maximum partition dimension of a tile
+    TILE_128 = nl.tile_size.pmax  # 128
+
     # Process the images in batches
     for b in nl.affine_range(batch_size):
         for output_tile_idx in nl.affine_range(n_tiles_c_out):
-            for input_row in nl.affine_range(out_pool_height//2):
-                # for input_col in nl.affine_range(out_width):
-                    X_out_tile_before_pooling = nl.zeros((c_in_pmax, 2, out_width),  X_out.dtype, buffer=nl.psum)
-                    # X_out_tile_after_pooling = nl.zeros((c_in_pmax,1),  X_out.dtype)
-                    for input_tile_idx in nl.affine_range(n_tiles_c_in):
-                        for i in nl.affine_range(filter_height):
-                            for j in nl.affine_range(filter_width):
-                                shifted_row = 2*input_row*pool_size+i
-                                X_rhs_tile = nl.load(X[b,c_in_pmax*input_tile_idx:c_in_pmax*(input_tile_idx+1), shifted_row:shifted_row+2, :])
-                                # X_rhs_tile_shifted = X_rhs_tile.reshape((128, 32))[in_channels_idx, out_height_idx*input_width+(out_width_idx+j)]
-                                X_rhs_tile_shifted = X_rhs_tile[in_channels_idx, nl.arange(2)[None,:,None], out_width_idx+j]
-                                # X_rhs_tile_shifted = X_rhs_tile_shifted.reshape((in_channels, out_width*2))
-                                W_tile = nl.load(W[c_in_pmax*output_tile_idx:c_in_pmax*(output_tile_idx+1), c_in_pmax*input_tile_idx:c_in_pmax*(input_tile_idx+1)])
-                                X_out_tile_before_pooling += nl.matmul(W_tile[:,:,i,j], X_rhs_tile_shifted, transpose_x=False)
-                    # X_out_tile_after_pooling = nl.copy(X_out_tile_before_pooling)
-                    # nl.max(X_out_tile_after_pooling.reshape(c_in_pmax, pool_size, 2/pool_size, out_width))
-                    X_out_tile_before_pooling_with_bias = (nl.load(bias[output_tile_idx*c_in_pmax:(output_tile_idx+1)*(c_in_pmax),])+X_out_tile_before_pooling)
-                    X_out_tile_after_pooling = nl.max(nl.max(X_out_tile_before_pooling_with_bias.reshape((c_in_pmax, pool_size, 2/pool_size, out_width)), axis=1).reshape((c_in_pmax, 2/pool_size, pool_size, out_width/pool_size)), axis=2)
-                    # X_out_tile_after_pooling = nl.max(nl.max(X_out_tile_before_pooling.reshape((128, pool_size, 2/pool_size, 222)), axis=1).reshape((128, 2/pool_size, pool_size, out_width/pool_size)), axis=2)
-                    # pdb.set_trace()
-                    nl.store(X_out[b, c_in_pmax*output_tile_idx:c_in_pmax*(output_tile_idx+1), 2*input_row:(2*input_row+2)], value=X_out_tile_after_pooling)
-        # pdb.set_trace()
-        # nl.store(X_out[b], value=X_b[out_channels_idx, out_pool_height_idx, out_pool_width_idx])
+            partial_sum = nl.zeros((TILE_128, out_height*out_width), X_out.dtype, buffer=nl.psum)
+            for input_tile_idx in nl.affine_range(n_tiles_c_in):
+                W_tile = nl.ndarray((TILE_128, TILE_128, filter_height, filter_width), dtype=W.dtype, buffer=nl.sbuf)
+                for i in nl.affine_range(filter_height):
+                    for j in nl.affine_range(filter_width):
+                        X_tile = nl.ndarray((TILE_128, input_height, input_width), dtype=X.dtype, buffer=nl.sbuf)
+                        X_tile[...] = nl.load(X[b, input_tile_idx*c_in_pmax:(input_tile_idx+1)*c_in_pmax])
+                        X_tile_shifted = nl.copy(X_tile)[in_channels_idx, out_height_idx+i, out_width_idx+j]
+                        X_tile_reshaped = nl.copy(X_tile_shifted).reshape((TILE_128, out_height*out_width))
+                        W_tile[...] = nl.load(W[output_tile_idx*c_in_pmax:(output_tile_idx+1)*c_in_pmax, input_tile_idx*c_in_pmax:(input_tile_idx+1)*c_in_pmax])
+                        partial_sum += nl.matmul(nl.copy(W_tile[:,:,i,j]), X_tile_reshaped, transpose_x=False)
+            temp = nl.copy(partial_sum).reshape((c_in_pmax, out_height, out_width))
+            nl.store(X_out[b, output_tile_idx*c_in_pmax:(output_tile_idx+1)*c_in_pmax], value=temp[out_channels_idx, out_height_idx, out_width_idx])
     return X_out
-
 
 def nki_matmul_tiled_(lhsT, rhs, result):
   """NKI kernel to compute a matrix multiplication operation in a tiled manner"""
